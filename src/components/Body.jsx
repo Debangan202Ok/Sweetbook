f1d75465dcebb1f63e5b9e919617abd9c61e37d2
paul <98002712+pauldebangan1970@users.noreply.github.com>
2023-07-09 18:29:13 +0530

    Banner added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,13 +2,13 @@ import Midarea from "./Midarea"
 import RightNavbar from "./RightNavbar"
 import Sidebar from "./Sidebar"
 import PropTypes from 'prop-types'
-
+import banner from "../../public/images/banner.png"
 
 const Body = ({top}) => {
   return (
     <div className="flex flex-col w-full  h-[80vh] md:[65vh] overflow-scroll overflow-x-hidden  border-8 border-t-0 border-b-0 border-[#898F9C] bg-white m-auto">
       <div ref={top} id="banner" className="flex w-full h-fit justify-center bg-[#12306d]">
-        <img src="../../public/images/banner.png" alt="" />
+        <img src={banner} alt="" />
       </div>
       <div className="flex flex-col md:flex-row items-center md:items-baseline md:justify-between">
         <Sidebar />
